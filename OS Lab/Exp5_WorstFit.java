8a3eb36477f0e480ea1c748ca0f19b27d8e35d81
Chandrabhanu-Prusty <prustycb18@gmail.com>
2026-04-27 21:28:52 +0530

    worst fit

---
+++ OS Lab/Exp5_WorstFit.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Exp5_WorstFit {
+    public static void main(String[] args) {
+        int[] block = {100, 500, 200, 300, 600};
+        int[] process = {212, 417, 112, 426};
+
+        int[] alloc = new int[process.length];
+        Arrays.fill(alloc, -1);
+
+        for (int i = 0; i < process.length; i++) {
+            int worst = -1;
+
+            for (int j = 0; j < block.length; j++)
+                if (block[j] >= process[i] &&
+                   (worst == -1 || block[j] > block[worst]))
+                    worst = j;
+
+            if (worst != -1) {
+                alloc[i] = worst;
+                block[worst] -= process[i];
+            }
+        }
+
+        System.out.println("P\tSize\tBlock");
+        for (int i = 0; i < process.length; i++)
+            System.out.println((i+1)+"\t"+process[i]+"\t"+
+                (alloc[i]==-1 ? "NA" : alloc[i]+1));
+    }
+}
